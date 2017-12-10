89d13d257e90afcd261cc6c2ee2a14315c25d27a
Ian <ianw3214@gmail.com>
2017-12-09 22:13:14 -0500

    Finish day 1 challenge 2

--- solutions/day1-002.cpp
+++ solutions/day1-002.cpp
@@ -7,10 +7,21 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    // first we have to get the length of the argument
     char *sequence = argv[1];
-    int sum = 0;
+    int length = 0;
     while (*sequence != '\0') {
-        
+        length++;
+        sequence++;
+    }
+
+    sequence = argv[1];
+    int sum = 0;
+    // don't worry too much about divide by 2 because we are guaranteed even number
+    for (int i = 0; i < length / 2; ++i) {
+        if (*(sequence + i) == *(sequence + length / 2 + i)) {
+            sum += (*(sequence + i) - 48) * 2;
+        }
     }
 
     std::cout << "The solution is: " << sum << '\n';
